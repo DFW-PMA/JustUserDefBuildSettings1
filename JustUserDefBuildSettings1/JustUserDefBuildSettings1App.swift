1dda64f8a9faffe081fb96f95c39944155b1148a
Daryl R Cox <dcox@justmacapps.net>
2024-08-21 15:21:03 -0500

    Initial Commit

---
+++ JustUserDefBuildSettings1/JustUserDefBuildSettings1App.swift
@@ -0,0 +1,17 @@
+//
+//  JustUserDefBuildSettings1App.swift
+//  JustUserDefBuildSettings1
+//
+//  Created by Daryl Cox on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct JustUserDefBuildSettings1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
